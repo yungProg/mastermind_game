3c48b671009e6d61ec25ad3816293bda16888faa
yungProg <gboakye007@outlook.com>
2025-02-11 17:46:23 +0000

    require computer_player class

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@
 require_relative 'lib/board'
 require_relative 'lib/game'
 require_relative 'lib/player'
+require_relative 'lib/computer_player'
 
 Game.new('Gideon', 'Ama').play
 Game.new('Gideon', 'Ama').play while Game.new('Gideon', 'Ama').play_again?.downcase == 'yes'
